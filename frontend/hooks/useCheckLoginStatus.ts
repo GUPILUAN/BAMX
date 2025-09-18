5e0ee7844b5c2ae1228891a2f385949f4fed1124
alexito0307 <alexito030710@gmail.com>
2025-09-17 23:58:30 -0600

    fix: :bug: Changed useCheckLoginStatus functions for navigation

--- frontend/hooks/useCheckLoginStatus.ts
+++ frontend/hooks/useCheckLoginStatus.ts
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { getData } from "../functions/userKey";
+import { navigate, replace } from "@/functions/NavigationService";
 
 export default function useCheckLoginStatus(navigation: any) {
   useEffect(() => {
     const checkLoginStatus = async () => {
       const token = await getData("access");
       if (token) {
-        navigation.navigate("DashBoard");
+        navigate("Dashboard");
       } else {
-        navigation.navigate("Auth");
+        replace("Auth");
       }
     };
     checkLoginStatus();
